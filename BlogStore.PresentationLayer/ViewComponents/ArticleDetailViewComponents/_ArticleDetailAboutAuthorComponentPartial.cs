1b18851716b4751ec79559b4a6c9401bedc4f7ea
onur_unlu40 <onur_unlu40@hotmail.com>
2025-06-24 22:26:52 +0300

    Article eklemeleri

--- BlogStore.PresentationLayer/ViewComponents/ArticleDetailViewComponents/_ArticleDetailAboutAuthorComponentPartial.cs
+++ BlogStore.PresentationLayer/ViewComponents/ArticleDetailViewComponents/_ArticleDetailAboutAuthorComponentPartial.cs
@@ -6,10 +6,16 @@ namespace BlogStore.PresentationLayer.ViewComponents.ArticleDetailViewComponents
     public class _ArticleDetailAboutAuthorComponentPartial : ViewComponent
     {
         private readonly IArticleService _articleService;
+
+        public _ArticleDetailAboutAuthorComponentPartial(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
         public IViewComponentResult Invoke(int id)
         {
-            var values = _articleService.TGetAppUserByArticleId(id);
-            return View(values);
+            var value = _articleService.TGetAppUserByArticleId(id);
+            return View(value);
         }
     }
 }
--- BlogStore.PresentationLayer/ViewComponents/ArticleDetailViewComponents/_ArticleDetailCommentListComponentPartial.cs
+++ BlogStore.PresentationLayer/ViewComponents/ArticleDetailViewComponents/_ArticleDetailCommentListComponentPartial.cs
@@ -6,6 +6,12 @@ namespace BlogStore.PresentationLayer.ViewComponents.ArticleDetailViewComponents
     public class _ArticleDetailCommentListComponentPartial : ViewComponent
     {
         private readonly ICommentService _commentService;
+
+        public _ArticleDetailCommentListComponentPartial(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
         public IViewComponentResult Invoke(int id)
         {
             var values = _commentService.TGetCommentsByArticle(id);
